31173a1a155b1ab2d806c62a4504deaed4ba8f48
Deandre <gitlab.deandre@phial.xyz>
2024-01-04 18:29:43 -0800

    Cleanup item

--- src/routes/new-tier-list/Item.svelte
+++ src/routes/new-tier-list/Item.svelte
@@ -1,26 +1,23 @@
 <script>
-	// @ts-nocheck
-
-	import astra from '$lib/assets/astra.png';
-
+	/**
+	 * @type {number}
+	 */
 	export let index;
-	export let id =
-		Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-	export let name = 'World';
-	export let image = astra;
-
-	function handleDragStart(event) {
-		event.dataTransfer.setData('application/json', JSON.stringify({ id, index, type: 'item' }));
-	}
+	/**
+	 * @type {number}
+	 */
+	export let id;
+	/**
+	 * @type {string}
+	 */
+	export let name;
+	/**
+	 * @type {string}
+	 */
+	export let image;
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
-<div
-	class="tier-item flex flex-col justify-center items-center space-y-2"
-	draggable="true"
-	on:dragstart={handleDragStart}
-  id={id}
-  aria
->
+<div class="tier-item flex flex-col justify-center items-center space-y-2">
 	<img class="w-28 h-28 object-contain" src={image} alt={name} />
 </div>
